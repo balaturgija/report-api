5d137ae2d8b5dfe1395b2f2bad82c06142f85b63
balaturgija <matej.balat@gmail.com>
2023-02-17 13:54:53 +0100

    Create dummy guard and roll for auth w roles

--- src/user/decorators/role.decorator.ts
+++ src/user/decorators/role.decorator.ts
@@ -1,6 +1,4 @@
 import { SetMetadata } from '@nestjs/common';
 import { UserType } from '@prisma/client';
 
-export const Roles = (...roles: UserType[]) => {
-  SetMetadata('roles', roles);
-};
+export const Roles = (...roles: UserType[]) => SetMetadata('roles', roles);
